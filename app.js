72e3124e20602c2ad72fdf2e9382ac460ecd771e
Angel Galindo <ajga203@gmail.com>
2024-10-27 21:35:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//function that calculates human to dog years
+
+//variables declared
+const myAge = 24;
+let earlyYears = 2;
+
+//multiplies and saves the value in the variable
+earlyYears *= 10.5;
+
+//declares a variable that holds the subtracted years of my age
+
+let laterYears = myAge - 2;
+
+//multiplies and saves the laterYears by 4
+laterYears *= 4;
+
+/* check if the equations are correct
+  console.log(earlyYears);
+  console.log(laterYears);
+*/
+
+//storing the summ of early and later years in a variable
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+//writing my name as a string and storing it in a variable
+
+let myName = 'Angel';
+
+//converts the myName variable to lowercase
+ myName = myName.toLowerCase();
+
+//logging all the variables in a string for results
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+
+
+
+
+
+
+
+
+
+
